a48f7d46ccb7d867d8ae1f60f4d6e78475126a08
AshanBaig <147313563+AshanBaig@users.noreply.github.com>
2024-06-06 11:42:38 +0500

    6-June-2024

---
+++ 846. Hand of Straights.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize==0:
+            hand.sort()
+            while True:
+                if len(hand)==0:
+                    return True
+                n=hand[0]
+                if len(hand)==0:
+                    return True
+                for i in range(groupSize):
+                    if n+i in hand:
+                        hand.remove(n+i)
+                    else:
+                        return False
+        return False
