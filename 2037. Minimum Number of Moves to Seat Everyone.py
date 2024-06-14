765b687e2a723170de77090e4f02492c8354cf53
AshanBaig <147313563+AshanBaig@users.noreply.github.com>
2024-06-14 23:25:25 +0500

    6-14-2024

---
+++ 2037. Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,11 @@
+#2037git add .
+class Solution:
+    def minMovesToSeat(self, seats, students) -> int:
+        seats.sort()
+        students.sort()
+        move=0
+        for i in range(len(students)):
+            move+=abs(students[i]-seats[i])
+        return move
+
+        
