9fd053c222a1ced73e0ce3949387b2f38b77e9a6
AshanBaig <147313563+AshanBaig@users.noreply.github.com>
2024-08-01 14:18:19 +0500

    1st-August-2024

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,45 +1,15 @@
 class Solution:
+    def append_s(self,n):
+        self.s+=self.d[n]
+        self.num-=n
     def intToRoman(self, num: int) -> str:
-        d={1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
-        l=[1,5,10,50,100,500,1000]
-        s=''
-        while num:
-            
-           if str(num)[0]=='4' or str(num)[0]=='9':
-            for i in range(len(l)-1):
-                if l[i]<int(str(num)[0])<l[i+1]:
-                    if int(str(num)[0])==9:
-                        s+=d[l[i-1]*10**(len(str(num))-1)]
-                    else:
-                        s+=d[l[i]*10**(len(str(num))-1)]
-                    s+=d[l[i+1]*10**(len(str(num))-1)]
-                    num=str(num)[::-1][:len(str(num))-1][::-1]
-                    if num=='':
-                        return s
-                    else:
-                        num=int(num)
+        self.s=''
+        self.num=num
+        self.d={1000:'M',900:'CM',500: "D",400: "CD",100: "C",90: "XC",50:'L',40: "XL",10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+        while self.num:
+            for i in self.d:
+                if self.num>=i:
+                    self.append_s(i)
                     break
-            continue
-                    
-           if num>999:
-            s+='M'
-            num-=1000
-           elif num>499:
-            s+='D'
-            num-=500
-           elif num>99:
-            s+='C'
-            num-=100
-           elif num>49:
-            s+="L"
-            num-=50
-           elif num>9:
-            s+='X'
-            num-=10
-           elif num>4:
-            s+='V'
-            num-=5
-           else:
-            s+='I'
-            num-=1
-        return s
+        return self.s
+
---
+++ 15. 3Sum.py
@@ -0,0 +1,3 @@
+l=[[1,2],[2,1]]
+l.sort()
+print(list(set(l)))
