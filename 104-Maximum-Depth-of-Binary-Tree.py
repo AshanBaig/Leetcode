cca16b5adcf5df3a844311e86f8211542ef3cf63
ashanbaig <147313563+AshanBaig@users.noreply.github.com>
2024-11-30 13:46:02 +0500

    [104] [Time Beats: 5.67%] [Memory Beats: 23.33%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.py
+++ 104-Maximum-Depth-of-Binary-Tree.py
@@ -1,3 +1,4 @@
+from queue import Queue
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,10 +7,24 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def height(node,count):
-            if node==None:
-                return 0
-            l=height(node.left,count+1)+1
-            r=height(node.right,count+1)+1
-            return max(l,r)
-        return height(root,0)
+        # def height(node,count):
+        #     if node==None:
+        #         return 0
+        #     l=height(node.left,count+1)+1
+        #     r=height(node.right,count+1)+1
+        #     return max(l,r)
+        # return height(root,0)
+        if root==None:
+            return 0
+        q=Queue()
+        level=0
+        q.put(root)
+        while not(q.empty()):
+            for i in range(q.qsize()):
+                node=q.get()
+                if node.left!=None:
+                    q.put(node.left)
+                if node.right!=None:
+                    q.put(node.right)
+            level+=1
+        return level
